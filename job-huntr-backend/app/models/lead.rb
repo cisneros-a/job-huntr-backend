0c6f41c49f38f3ddaa373dd1ed9bebf3037d4fd7
Farmwell <g.ferowich@gmail.com>
2020-01-21 15:57:50 -0500

    wrote validations for users and leads

--- job-huntr-backend/app/models/lead.rb
+++ job-huntr-backend/app/models/lead.rb
@@ -1,4 +1,5 @@
 class Lead < ApplicationRecord
   belongs_to :user
   has_many :notes
+  validates :company, uniqueness: true
 end
--- job-huntr-backend/app/models/user.rb
+++ job-huntr-backend/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :leads
+    validates :email, uniqueness: true
+    validates :name, presence: true
+    validates :password, presence: true
 end
