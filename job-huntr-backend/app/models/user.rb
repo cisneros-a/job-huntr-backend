815305daf3bda53ea8618f4033d4058242af879a
Farmwell <g.ferowich@gmail.com>
2020-01-21 18:34:30 -0500

    added auth gems

--- job-huntr-backend/app/models/user.rb
+++ job-huntr-backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password 
     has_many :leads
-    validates :email, uniqueness: true
-    validates :name, presence: true
-    validates :password, presence: true
+    # validates :email, uniqueness: true
+    # validates :name, presence: true
+    # validates :password, presence: true
 end
