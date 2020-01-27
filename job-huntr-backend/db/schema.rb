2d4ecd5f9be258251a963c00f372eeb923f59cf1
Farmwell <g.ferowich@gmail.com>
2020-01-27 12:55:07 -0500

    Procfile

--- job-huntr-backend/db/schema.rb
+++ job-huntr-backend/db/schema.rb
@@ -10,39 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_212420) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "leads", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "title"
-    t.string "company"
-    t.string "location"
-    t.string "status"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_leads_on_user_id"
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.bigint "lead_id", null: false
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["lead_id"], name: "index_notes_on_lead_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "leads", "users"
-  add_foreign_key "notes", "leads"
 end
